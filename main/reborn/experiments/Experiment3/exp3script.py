25d20df34d9f6443ea33761a302851f6209f7441
jinfenglin <ljfnwpusife@gmail.com>
2019-07-21 17:15:53 -0400

    fix

--- main/reborn/experiments/Experiment3/exp3script.py
+++ main/reborn/experiments/Experiment3/exp3script.py
@@ -25,8 +25,7 @@ if __name__ == "__main__":
     #models = ["vsm", "gvsm", "lda"]
     models = ["lsi"]
     use_translate_flags = [False, True]
-    #languages = args.languages
-    languages = ["fr"]
+    languages = args.languages
     time = datetime.datetime.now().strftime(
         "%Y%m%d-%H%M%S")  # TODO modify the output dir to make result group by langauge
     for language in languages:
